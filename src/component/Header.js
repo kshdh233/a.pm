7c026a53ea0cf679007c69b7304bbd39611aa6fc
Kshdh23 <138372630+Kshdh23@users.noreply.github.com>
2023-07-12 14:20:49 +0900

    7//12

---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+function Header(){
+  return <header>
+    <h1> a.pm</h1>
+    </header>
+}
+
+export default Header;
---
+++ src/component/Schedule.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+function Schedule(){
+  return(
+    <div className='box-area'>
+    <a href="/schedule">일정 관리 {'>'}</a>
+    </div>
+  )
+};
+
+export default Schedule;
