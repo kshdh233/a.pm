ad1f392adbe9cb13dd428da8cbc8d6ecca59aab0
Kshdh23 <138372630+Kshdh23@users.noreply.github.com>
2023-07-29 21:10:26 +0900

    07-29 Carousel

---
+++ src/component/Carousel.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
+import images from "./images";
+
+const renderSlides = images.map(image => (
+  <div key={image.alt} className="flex justify-center items-center">
+    <img src={image.url} alt={image.alt} />
+  </div>
+));
+
+const Carousell = () => {
+  const [currentIndex, setCurrentIndex] = useState();
+
+  function handleChange(index) {
+    setCurrentIndex(index);
+  }
+
+  return (
+    <div className="flex justify-center items-center py-5 px-3">
+      <div style={{ maxHeight: '50vh' /* Set the desired max height here */ }}>
+        <Carousel
+          showArrows={false}
+          autoPlay={true}
+          infiniteLoop={true}
+          showThumbs={false}
+          selectedItem={images[currentIndex]}
+          onChange={handleChange}
+          className="w-[400px] lg:hidden"
+        >
+          {renderSlides}
+        </Carousel>
+      </div>
+    </div>
+  );
+};
+
+export default Carousell;
--- src/page/Play_infor_home_page.js
+++ src/page/Play_infor_home_page.js
@@ -2,8 +2,8 @@ import * as React from 'react';
 import Header from '../component/Header';
 import FixedBottomNavigation from '../component/BottomNav';
 import SearchBar from "../component/SearchBar";
+import Carousell from "../component/Carousel";
 import { Link } from 'react-router-dom';
-import '../styles/Play_info_home_page.css'
 
 function Play_infor_home_page() {
   return (
@@ -13,24 +13,7 @@ function Play_infor_home_page() {
       <SearchBar/>
 
       {/* Main Content */}
-      <div style={{ flex: 1 }}>
-        <div>인기 공연 TOP5</div>
-        <Link to="/Play_information_page">
-          <button className='Pop-Play'>베르베르</button>
-        </Link>
-        <Link to="/Play_information_page">
-          <button className='Pop-Play'>베르베르</button>
-        </Link>
-        <Link to="/Play_information_page">
-          <button className='Pop-Play'>베르베르</button>
-        </Link>
-        <Link to="/Play_information_page">
-          <button className='Pop-Play'>베르베르</button>
-        </Link>
-        <Link to="/Play_information_page">
-          <button className='Pop-Play'>베르베르</button>
-        </Link>
-      </div>
+      <Carousell />
 
       {/* Footer */}
       <FixedBottomNavigation />
