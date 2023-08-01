e99236735d9c071ffc182db3245d2f303a942004
Kshdh23 <138372630+Kshdh23@users.noreply.github.com>
2023-08-01 23:06:25 +0900

    캐러솔 변경

--- src/component/Carousel.js
+++ src/component/Carousel.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from "react-responsive-carousel";
 import images from "./images";
+import { Link } from 'react-router-dom';
 
 const renderSlides = images.map(image => (
   <div key={image.alt} className="flex justify-center items-center">
@@ -18,6 +19,7 @@ const Carousell = () => {
 
   return (
     <div className="flex justify-center items-center py-5 px-3">
+      <Link to="/Play_information_page">
       <div style={{ maxHeight: '50vh' /* Set the desired max height here */ }}>
         <Carousel
           showArrows={false}
@@ -31,6 +33,7 @@ const Carousell = () => {
           {renderSlides}
         </Carousel>
       </div>
+      </Link>
     </div>
   );
 };
