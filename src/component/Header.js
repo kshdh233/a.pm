581d38a43fa20d295495dc9a6b61d42cc1115172
Kshdh23 <138372630+Kshdh23@users.noreply.github.com>
2023-08-02 17:27:20 +0900

    08-02

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 function Header(){
   return <header>
-    <h1> a.pm</h1>
+    <Link to="/Main_page">
+      <h1> a.pm</h1>
+    </Link>
     </header>
 }
 
