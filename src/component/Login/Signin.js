2a9b6da24c4f963dedf328bd49f82b88198d993a
Kshdh23 <138372630+Kshdh23@users.noreply.github.com>
2023-08-06 11:08:39 +0900

    폴더 수정.

--- src/component/Login/Signin.js
+++ src/component/Login/Signin.js
@@ -1,13 +1,12 @@
 import React, { useState } from 'react';
-// import { useDispatch } from 'react-redux';
-// import { loginUser } from '../../../_actions/user_action';
+import { Link, useNavigate } from 'react-router-dom';
 
-function Signin() {
 
-  // const dispatch = useDispatch();
-
+function Login(props) {
   const [ID, setID] = useState("");
   const [Password, setPassword] = useState("");
+
+  const navigate = useNavigate();
 
   const onIDHandler = (event) => {
     setID(event.currentTarget.value);
@@ -17,39 +16,59 @@ function Signin() {
     setPassword(event.currentTarget.value);
   }
 
-  const onSubmitHandler = (event) => {
-    // Prevent the page from reloading on button click
+  const onSubmitHandler = async (event) => {
     event.preventDefault();
 
-    console.log('ID:', ID);
-    console.log('Password:', Password);
+    try {
+      // 서버로 로그인 정보를 전송하는 API 호출
+      const response = await fetch('/user/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: ID,
+          password: Password,
+        }),
+      });
 
-    let body = {
-      id: ID,
-      password: Password,
-    };
-    // dispatch(loginUser(body));
-  }
+      // 서버로부터 받은 응답을 JSON 형태로 파싱
+      const data = await response.json();
+
+      if (response.status === 200) {
+        // 로그인 성공 시 홈 페이지로 이동
+        navigate('/Main_page');
+      } else {
+        // 로그인 실패 시 에러 메시지를 출력
+        alert(data.message);
+      }
+    } catch (error) {
+      console.error('Error during login:', error);
+      alert('서버 오류가 발생했습니다.');
+    }
+  };
 
   return (
     <div style={{
-      display: 'flex', justifyContent: 'center', alignItems: 'center',
+      display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center',
       width: '100%', height: '400px'
     }}>
-      <form style={{ display: 'flex', flexDirection: 'column' }}
-        onSubmit={onSubmitHandler}
-      >
+      <form style={{ display: 'flex', flexDirection: 'column' }} onSubmit={onSubmitHandler}>
         <label>ID</label>
         <input type='text' value={ID} onChange={onIDHandler} />
+
         <label>비밀번호</label>
         <input type='password' value={Password} onChange={onPasswordHandler} />
+        
         <br />
-        <button formAction=''>
+        <button type='submit'>
           로그인
         </button>
       </form>
+      <br />
+      <Link to="/signup">회원가입 페이지로 이동</Link>
     </div>
   )
 }
 
-export default Signin;
+export default Login;
