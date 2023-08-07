62230ada43638afe8d37019471ba901cc6470b49
Kshdh23 <138372630+Kshdh23@users.noreply.github.com>
2023-08-07 13:02:30 +0900

    .

--- src/component/TheaterList.js
+++ src/component/TheaterList.js
@@ -1,43 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import TheaterDetail from './TheaterDetail';
 
 const TheaterList = () => {
   const [theaterList, setTheaterList] = useState([]);
-  const [selectedTheaterId, setSelectedTheaterId] = useState(null);
 
   useEffect(() => {
-    loadTheaterList();
+    // 서버에서 데이터 가져오는 함수 정의
+    const fetchTheaters = async () => {
+      try {
+        // Axios를 이용하여 서버에서 극장 목록 가져오기
+        const response = await axios.get('/theater/list');
+
+        // 서버에서 받아온 데이터 중에서 서울 지역인 극장만 필터링하여 React 상태에 저장
+        setTheaterList(response.data);
+      } catch (error) {
+        console.error('Error fetching theaters:', error);
+      }
+    };
+
+    // 함수 호출
+    fetchTheaters();
   }, []);
-
-  const loadTheaterList = async () => {
-    try {
-      const response = await axios.get('/theater/list');
-      setTheaterList(response.data);
-    } catch (error) {
-      console.error('Error loading theater list:', error);
-    }
-  };
-
-  const handleTheaterClick = (theaterId) => {
-    setSelectedTheaterId((prevSelectedId) => (prevSelectedId === theaterId ? null : theaterId));
-  };
-
+  
   return (
     <div>
-      <h1>극장 목록</h1>
+      <h2>서울 지역 극장 목록</h2>
       <ul>
-        {theaterList.map((theater) => (
-          <li
-            key={theater.theaterId}
-            onClick={() => handleTheaterClick(theater.theaterId)}
-            style={{ cursor: 'pointer' }}
-          >
-            {theater.theaterName}
+        {theaterList.map(theater => (
+          <li key={theater.theaterId}>
+            <p>극장명: {theater.theaterName}</p>
+            <p>극장 위치: {theater.theaterLocation}</p>
+            {/* 여기에 필요한 정보를 추가로 표시하거나 수정 가능 */}
           </li>
         ))}
       </ul>
-      {selectedTheaterId && <TheaterDetail theaterId={selectedTheaterId} />}
     </div>
   );
 };
