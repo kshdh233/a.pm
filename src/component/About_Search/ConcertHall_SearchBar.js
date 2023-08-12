03cff51d81645d9ecafdf3b8a8510dff1ccf3a6c
chanAI-20 <lscstar0601@gmail.com>
2023-08-12 19:24:15 +0900

    1. signin api 변경

--- src/component/About_Search/ConcertHall_SearchBar.js
+++ src/component/About_Search/ConcertHall_SearchBar.js
@@ -1,45 +1,49 @@
 import React, { useEffect, useState } from "react";
-import { Search } from "@mui/icons-material"; // Google Material Icons의 돋보기 아이콘 임포트
-import "../../styles/SearchBar.css";
 import { Autocomplete, TextField } from "@mui/material";
 import axios from "axios";
-
-
 
 const ConcertHall_SearchBar = (props) => {
   const [searchValue, setSearchValue] = useState("");
   const [theaterList, setTheaterList] = useState([]);
-
-  useEffect(()=>{
-    const fetchDataFromServer = async() =>{
-      try{
-        const response = await axios.get("/theater/list/seoul?page=1");
+  const [filteredTheaterList, setFilteredTheaterList] = useState([]);
+  
+  useEffect(() => {
+    const fetchDataFromServer = async () => {
+      try {
+        const response = await axios.get("/theater/list/seoul");
         const theaterData = response.data.content;
         setTheaterList(theaterData);
         console.log(theaterData);
-      }catch(error){
-        console.error("Error fetching data : ",error);
+      } catch (error) {
+        console.error("Error fetching data: ", error);
       }
     };
 
     fetchDataFromServer();
-  })
+  }, []);
 
   const handleInputChange = (event, newValue) => {
     setSearchValue(newValue);
-    // value로 공연장 전체 데이터 filtering 해서 자동완성에 보여주기
+
+    // 검색어에 따라 옵션 목록을 필터링
+    const filteredTheaterList = theaterList.filter((theater) =>
+      theater.theaterName.toLowerCase().includes(newValue.toLowerCase())
+    );
+
+    // 필터링된 목록을 설정
+    setFilteredTheaterList(filteredTheaterList);
   };
 
   return (
     <div>
       <div>
         <Autocomplete
-          options={theaterList.map((theater)=>theater.theaterName)}
+          options={filteredTheaterList.map((theater) => theater.theaterName)}
           placeholder="공연장 검색하기"
-          onChange={(event, newValue) => handleInputChange(event,newValue)}
+          onChange={(event, newValue) => handleInputChange(event, newValue)}
           value={searchValue}
           renderInput={(params) => (
-            <TextField {...params} label="공연장 검색하기"/>
+            <TextField {...params} label="공연장 검색하기" />
           )}
         />
       </div>
--- src/component/About_Search/PickUp.js
+++ src/component/About_Search/PickUp.js
@@ -11,15 +11,10 @@ const SearchPage = () => {
   useEffect(() => {
     const fetchDataFromServer = async () => {
       try {
-        const response = await axios.get("/theater/list/seoul?page=7");
+        const response = await axios.get("/theater/list/seoul");
         const theaterData = response.data.content; // content 배열을 사용합니다.
-        console.log(theaterData);
-        if (!Array.isArray(theaterData) || theaterData.length === 0) {
-          console.error("Invalid theater data:", theaterData);
-          // 적절한 오류 처리를 추가하세요 (예: 사용자에게 오류 메시지 표시)
-          return;
-        }
         setTheaterList(theaterData);
+        console.log(theaterData);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
