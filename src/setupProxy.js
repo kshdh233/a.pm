68ef295c08e75d7d361fe6c4d4157abd9db3da3b
Skadi <74538745+SeokBaeJeon@users.noreply.github.com>
2023-08-14 11:56:27 +0900

    Update setupProxy.js

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,14 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function(app) {
   app.use(
-    '/',
+    [
+      '/actor/*', 
+      '/pmshow/*', 
+      '/seat/*', 
+      '/theater/*', 
+      '/user/*', 
+      '/view/*'
+    ],
     createProxyMiddleware({
       target: 'https://apm-backend-a20e349efc23.herokuapp.com',
       changeOrigin: true,
